cb089ad808fb40ecf7e55b72b8713a4ab989808e
Abu09127abu <ojopa38@gmail.com>
2024-11-19 21:03:48 +0600

    Update server.js

--- server.js
+++ server.js
@@ -1,12 +1,12 @@
-// server.js (Node.js Backend)
-const express = require('express');
-const path = require('path');
+const express = require("express");
+const path = require("path");
+
 const app = express();
-
-// Serve the index.html file
-app.use(express.static(path.join(__dirname, 'public')));
-
 const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => {
-  console.log(`Server is running on http://localhost:${PORT}`);
-});
+
+app.use(express.static(path.join(__dirname, "public")));
+
+app.get("/", (req, res) => res.sendFile(path.join(__dirname, "index.html")));
+app.get("/admin", (req, res) => res.sendFile(path.join(__dirname, "admin.html")));
+
+app.listen(PORT, () => console.log(`Server running on http://localhost:${PORT}`));
