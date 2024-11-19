d5b4fba850405276e2faa26271a4cabc59a43cbb
Abu09127abu <ojopa38@gmail.com>
2024-11-19 21:44:52 +0600

    Update server.js

--- server.js
+++ server.js
@@ -1,12 +1,18 @@
 const express = require("express");
+const app = express();
 const path = require("path");
 
-const app = express();
+app.use(express.static("public"));
+
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "index.html"));
+});
+
+app.get("/admin", (req, res) => {
+  res.sendFile(path.join(__dirname, "admin.html"));
+});
+
 const PORT = process.env.PORT || 3000;
-
-app.use(express.static(path.join(__dirname, "public")));
-
-app.get("/", (req, res) => res.sendFile(path.join(__dirname, "index.html")));
-app.get("/admin", (req, res) => res.sendFile(path.join(__dirname, "admin.html")));
-
-app.listen(PORT, () => console.log(`Server running on http://localhost:${PORT}`));
+app.listen(PORT, () => {
+  console.log(`Server running at http://localhost:${PORT}`);
+});
